d3958907c48b38aef2e07e9b5c91db992a7c4a1a
taestii <91502810+taestii@users.noreply.github.com>
2023-02-04 17:45:59 -0500

    new file:   js-samples 	new file:   key.js 	modified:   notes.txt 	new file:   planning.txt 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
+// This example requires the Places library. Include the libraries=places
+// parameter when you first load the API. For example:
+// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
 let map;
+let service;
+let infowindow;
+var input;
+
+function search(){
+  input = document.getElementById("searchInput").value;
+
+}
 
 function initMap() {
+  const sydney = new google.maps.LatLng(-33.867, 151.195);
+
+  infowindow = new google.maps.InfoWindow();
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+    center: sydney,
+    zoom: 15,
+  }); //place map into html
+
+
+}
+
+function createMarker(place) {
+  if (!place.geometry || !place.geometry.location) return;
+
+  const marker = new google.maps.Marker({
+    map,
+    position: place.geometry.location,
+  });
+
+  google.maps.event.addListener(marker, "click", () => {
+    infowindow.setContent(place.name || "");
+    infowindow.open(map);
   });
 }
 
